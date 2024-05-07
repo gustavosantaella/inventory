6ee0727ae53d21e48e617d73717d6ab689df9db0
Gustavo Alejandro <llxsantaellaxll@gmail.com>
2024-05-07 11:20:40 -0400

    commit

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,8 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AuthModule } from './modules/auth/auth.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [AuthModule],
+  imports: [
+    AuthModule,
+    MongooseModule.forRoot('mongodb://localhost:27017/inventory'),
+  ],
   exports: [],
   providers: [],
 })
